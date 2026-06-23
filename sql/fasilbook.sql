7b1331c16a4616c3acbe5f5dd40334c3437eabfd
reinnlerr <birelramadhan@gmail.com>
2026-06-23 13:38:48 +0700

    up sql

--- sql/fasilbook.sql
+++ sql/fasilbook.sql
@@ -1,52 +1,73 @@
--- Database FasilBook Final
-CREATE DATABASE IF NOT EXISTS fasilbook;
-USE fasilbook;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Jun 2026 pada 08.36
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
 
-DROP TABLE IF EXISTS pesanan;
-DROP TABLE IF EXISTS lapangan;
-DROP TABLE IF EXISTS admin;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `lapangan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_lapangan` varchar(100) NOT NULL,
-  `lokasi` varchar(255) NOT NULL,
-  `harga_per_jam` int(11) NOT NULL,
-  `gambar` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `lapangan` (`id`, `nama_lapangan`, `lokasi`, `harga_per_jam`, `gambar`) VALUES
-(1, 'Arcamanik Futsal Camp', 'Jl. Arcamanik Endah No 15, Arcamanik, Bandung', 75000, 'img/lapangan_1.png'),
-(2, 'Cisaranten Futsal Corner', 'Jl. Cisaranten Kulon No 22, Arcamanik, Bandung', 60000, 'img/lapangan_2.png'),
-(3, 'Antapani Futsal Sederhana', 'Jl. Terusan Jakarta No 88, Antapani, Bandung', 65000, 'img/lapangan_3.png'),
-(4, 'Sukamiskin Sport Lapang', 'Jl. Sukamiskin No 40, Arcamanik, Bandung', 50000, 'img/lapangan_4.png'),
-(5, 'Endah Futsal Kampung', 'Jl. Endah Raya No 5, Arcamanik, Bandung', 55000, 'img/lapangan_5.png'),
-(6, 'Ujungberung Futsal Rakyat', 'Jl. Ujungberung Raya No 10, Ujungberung, Bandung', 45000, 'img/lapangan_6.png');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `pesanan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lapangan_id` int(11) NOT NULL,
-  `nama_pemesan` varchar(100) NOT NULL,
-  `tanggal_booking` date NOT NULL,
-  `jam_mulai` time NOT NULL,
-  `status_pembayaran` enum('Belum Bayar','Lunas') DEFAULT 'Belum Bayar',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`lapangan_id`) REFERENCES `lapangan`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Database: `fasilbook`
+--
 
--- Data Dummy untuk Simulasi Jadwal yang Terisi hari ini agar tombol otomatis "Disabled"
-INSERT INTO `pesanan` (`lapangan_id`, `nama_pemesan`, `tanggal_booking`, `jam_mulai`, `status_pembayaran`) VALUES
-(1, 'Tim Fauzil FC', CURDATE(), '09:00:00', 'Lunas'),
-(1, 'Hagi & Co', CURDATE(), '14:00:00', 'Belum Bayar'),
-(2, 'Anak-Anak IF Poltek SCI', CURDATE(), '19:00:00', 'Lunas');
+-- --------------------------------------------------------
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Struktur dari tabel `mitra`
+--
+
+CREATE TABLE `mitra` (
+  `id` int(11) NOT NULL,
+  `nama_mitra` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `paket` enum('Starter','Growth','Scale') NOT NULL DEFAULT 'Starter',
+  `komisi_persen` int(11) NOT NULL DEFAULT 8,
+  `status_akun` varchar(20) NOT NULL DEFAULT 'Aktif'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Akun Default: Username (admin), Password (admin) menggunakan bcrypt hash
-INSERT INTO `admin` (`username`, `password`) VALUES
-('admin', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
+--
+-- Dumping data untuk tabel `mitra`
+--
+
+INSERT INTO `mitra` (`id`, `nama_mitra`, `username`, `password`, `paket`, `komisi_persen`, `status_akun`) VALUES
+(1, 'Futsal Arcamanik', 'mitra1', '123', 'Starter', 8, 'Aktif'),
+(2, 'Antapani Sport', 'mitra2', '123', 'Growth', 6, 'Aktif'),
+(3, 'Endah Futsal (Premium)', 'mitra3', '123', 'Scale', 4, 'Aktif'),
+(4, 'fu', 'mitra7', '123', 'Scale', 4, 'Aktif');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mitra`
+--
+ALTER TABLE `mitra`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mitra`
+--
+ALTER TABLE `mitra`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
